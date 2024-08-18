a1da4048a78e1619cc1b28665a6f6dd9b8b09b93
David Werfelmann <werfelmann@Davids-MBP.attlocal.net>
2024-08-17 19:23:20 -0500

    added styles and book images

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Awesome Books";
+   let book1 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1430001192i/90557.jpg";
+   let book2 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1600241424i/6288.jpg";
+   let book3 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1432468943i/70535.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Buddhism Without Beliefs by Stephen Batchelor" />
+         <img src={book2} alt="The Road by Cormac McCarthy" />
+         <img src={book3} alt="2001: A Space Odyssey by Arthur C. Clarke" />
       </div>      
    );
 }
